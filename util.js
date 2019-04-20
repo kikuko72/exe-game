3db7e7425e211147f659721e98342e17abc34473
kikuko72 <6210037@gmail.com>
2019-04-21 06:29:01 +0900

    Moveを(position, character)のMapで扱えるように、Pairを扱うMapを作成
    key - value が互いに1つの相手とのみ結びつくようにする

--- util.js
+++ util.js
@@ -6,12 +6,28 @@ class Patch {
     }
 
     applyTo(map) {
-        const altMap = new Map(map ? [...map.entries()] : [])
-        this.entries.forEach((e) => altMap.set(e[0], e[1]))
+        const oldEntries = map ? [...map.entries()] : []
+        const altMap = new Map(oldEntries)
+        this.entries.forEach(e => altMap.set(e[0], e[1]))
         return altMap
     }
 
     merge(another) {
         return new Patch(this.entries.concat(another.entries))
+    }
+}
+
+class PairingPatch {
+    constructor(entries) {
+        this.entries = entries
+        this.values = new Set()
+        entries.forEach(e => this.values.add(e[1]))
+    }
+
+    applyTo(map) {
+        const oldEntries = map ? [...map.entries()] : []
+        const altMap = new Map(oldEntries.filter(e => !this.values.has(e[1])))
+        this.entries.forEach(e => altMap.set(e[0], e[1]))
+        return altMap
     }
 }
